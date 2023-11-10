f993a8e14fff01d1bad675b5e2631d2c2649e705
Jethro Baring <baring.jeth@gmail.com>
2023-11-10 12:13:33 +0000

    fixed duplicate

--- src/lib/components/app/HowItWork.svelte
+++ src/lib/components/app/HowItWork.svelte
@@ -1,22 +1,4 @@
 
-<div class="h-screen flex items-center justify-center" id="hero">
-    <div class="flex items-center justify-center">
-      <div class="flex-1 flex flex-col gap-5">
-        <p class="text-7xl font-black">
-            A New Way to <br> Empower <br> <span class="text-flutter-blue">Startups</span>
-        </p>
-        <p class="text-xl font-medium">
-            Your startup's path to empowerment <br> and success
-        </p>
-        <button class="btn btn-custom btn-primary btn-custom border-none normal-case text-white bg-flutter-blue w-32 shadow-lg shadow-flutter-blue/50">
-            Apply Now!
-        </button>
-      </div>
-      <div class="flex-1">
-        <img src="startup.png" alt="startup" class="w-full" />
-      </div>
-    </div>
-  </div>
 <div class="pt-24"id="howitwork">
     <div class="flex flex-col gap-20 justify-center">
         <p class="font-extrabold text-4xl">How It Work</p>
